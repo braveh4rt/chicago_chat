fa5880e20be3e35397b375abfe8a9cc233412db2
Sebastian Skałacki <skalee@gmail.com>
2014-10-20 19:01:48 +0200

    Introduce room_state

--- src/websocket/chicago_chat_room_websocket.erl
+++ src/websocket/chicago_chat_room_websocket.erl
@@ -2,14 +2,15 @@
 -behaviour(boss_service_handler).
 -compile(export_all).
 
+-record(room_state, {clients, messages}).
+
 init() ->
   io:format(",,,,,,,,,,,, Chat server has started.~n", []),
-  {ok, []}.
+  {ok, init_room()}.
 
 handle_join(ServiceURL, WebSocket, State) ->
   io:format(",,,,,,,,,,,, New client has joined!~n", []),
-  WebSocket ! {text, server_message(<<"Welcome chatter!">>)},
-  {noreply, State}.
+  {noreply, add_client_to_room(State, WebSocket)}.
 
 handle_close(Reason, ServiceURL, WebSocket, State) ->
   {State, State}.
@@ -30,3 +31,24 @@ terminate(Reason, State) ->
 server_message(Body) ->
   Json = [{nick, server}, {body, Body}],
   mochijson2:encode(Json).
+
+% Should be moved to separate module but I'm in hurry.
+
+init_room() ->
+  #room_state{clients = [], messages = []}.
+
+add_client_to_room(Room, WebSocket) ->
+  NewClients = [WebSocket | Room#room_state.clients],
+  NewRoom = Room#room_state{clients = NewClients},
+  send_to_room(NewRoom, server_message(<<"Welcome chatter!">>)),
+  NewRoom.
+
+send_to_room(Room, Message) ->
+  send_to_many(Room#room_state.clients, Message).
+
+% Erlang allows simpler iteration via list module but I'm in hurry.
+send_to_many([WebSocket | Tail], Message) ->
+  WebSocket ! {text, Message};
+
+send_to_many([], _) ->
+  ok.
