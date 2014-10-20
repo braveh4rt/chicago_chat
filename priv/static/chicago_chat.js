0ea2169f22bcafef9b6a80db1643d8c8dadd80e4
Sebastian Skałacki <skalee@gmail.com>
2014-10-20 16:00:57 +0200

    Display messages in chat

--- priv/static/chicago_chat.js
+++ priv/static/chicago_chat.js
@@ -1,3 +1,14 @@
 $(function() {
-  alert("ready!");
+  $chat = $("#chat")
+
+  appendChat({nick: "Sebastian", body: "Hi there!"});
+  appendChat({nick: "Jodoz", body: "Make the chat!"});
+  appendChat({nick: "Sebastian", body: "Ok..."});
+
+  function appendChat(message) {
+    li = $("<li></li>");
+    li.append($("<span></span>").addClass("nick").text(message.nick));
+    li.append($("<span></span>").addClass("body").text(message.body));
+    $chat.append(li);
+  }
 })
