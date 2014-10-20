8c26d35c992d08478f35ca85d31dd5b102731022
Sebastian Skałacki <skalee@gmail.com>
2014-10-20 16:04:41 +0200

    Bit prettier stylesheets

--- priv/static/chicago_chat.js
+++ priv/static/chicago_chat.js
@@ -7,7 +7,7 @@ $(function() {
 
   function appendChat(message) {
     li = $("<li></li>");
-    li.append($("<span></span>").addClass("nick").text(message.nick));
+    li.append($("<span></span>").addClass("nick").text(message.nick + ": "));
     li.append($("<span></span>").addClass("body").text(message.body));
     $chat.append(li);
   }
