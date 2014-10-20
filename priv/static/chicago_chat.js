51e920a984919a7188ad1867b41d7420656a0ede
Sebastian Skałacki <skalee@gmail.com>
2014-10-20 16:45:17 +0200

    Setting nick

--- priv/static/chicago_chat.js
+++ priv/static/chicago_chat.js
@@ -1,14 +1,23 @@
 $(function() {
-  $chat = $("#chat")
+  $chat = $("#chat");
+  $nick = null;
 
   appendChat({nick: "Sebastian", body: "Hi there!"});
   appendChat({nick: "Jodoz", body: "Make the chat!"});
   appendChat({nick: "Sebastian", body: "Ok..."});
+
+  $("#nick").click(onNickClick);
 
   function appendChat(message) {
     li = $("<li></li>");
     li.append($("<span></span>").addClass("nick").text(message.nick + ": "));
     li.append($("<span></span>").addClass("body").text(message.body));
     $chat.append(li);
+  }
+
+  function onNickClick() {
+    $nick = $("#nick_text").val();
+    $("#nick_controls").hide();
+    $("#say_controls").show();
   }
 })
